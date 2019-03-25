f97364c10582f5dd26d8bc3f880061618aa032f6
Khitagarden <48733662+Khitagarden@users.noreply.github.com>
2019-03-25 19:14:08 -0300

    Add files via upload

---
+++ Atividade1-Cristiane.py
@@ -0,0 +1,33 @@
+# atividade, criar um banco de dados
+
+
+nome = 'Cristiane'
+s_nome = 'Ruedieger'
+idade = '30 anos'
+identidade = '23234343'
+cpf = '000.234.345.45'
+rua = 'Servidao Oliveira, 188'
+bairro = 'Cachoeira do Bom Jesus'
+cidade = 'FLorianopolis'
+estado = 'SC'
+telefone = '(48)99616-2014'
+escolaridade = 'Superior Completo'
+cargo = 'Designer de moda'
+salario_pretendido = 'A combinar'
+
+
+# saida
+
+print('PORTIFÓLIO')
+print()
+print(('Cargo pretendido: {}').format(cargo))
+print(('Salario Pretendido:{}').format(salario_pretendido))
+print()
+print('DADOS PESSOAIS')
+print(('NOME COMPLETO: {} {}').format(nome, s_nome))
+print()
+print(('ENDERECO: {} {} {} {}').format(rua, bairro, cidade, estado))
+print()
+print('FORMACAO')
+print()
+print(('ESCOLARIDADE: {}').format(escolaridade))
